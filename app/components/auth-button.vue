4d1156986ede20b5d115044f3a26b49920012607
Abdul <axah37@gmail.com>
2025-09-20 16:29:52 -0600

    add signout button

--- app/components/auth-button.vue
+++ app/components/auth-button.vue
@@ -17,6 +17,7 @@ const authStore = useAuthStore();
     <ul tabindex="0" class="dropdown-content menu bg-base-300 rounded-box z-1 w-52 p-2 shadow-sm">
       <li>
         <NuxtLink to="/sign-out">
+          <Icon name="tabler:logout-2" size="24" />
           Sign Out
         </NuxtLink>
       </li>
