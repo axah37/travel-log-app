373bbbd14669cff6d7d378ca0ab136d93cee1e09
Abdul Ahmed <axah37@gmail.com>
2025-09-20 16:31:19 -0600

    17 18 as a user i want to see my authentication status in the UI (#56)
    * #17 #18 logout
    
    * add signout button

--- app/components/auth-button.vue
+++ app/components/auth-button.vue
@@ -5,7 +5,26 @@ const authStore = useAuthStore();
 </script>
 
 <template>
-  <button :disabled="authStore.loading" class="btn btn-accent" @click="authStore.signIn()">
+  <div v-if="!authStore.loading && authStore.user" class="dropdown dropdown-end">
+    <div tabindex="0" role="button" class="btn m-1">
+      <div class="avatar">
+        <div class="w-8 rounded-full">
+          <img :src="authStore.user.image || undefined" :alt="authStore.user.name">
+        </div>
+      </div>
+      {{ authStore.user.name }}
+    </div>
+    <ul tabindex="0" class="dropdown-content menu bg-base-300 rounded-box z-1 w-52 p-2 shadow-sm">
+      <li>
+        <NuxtLink to="/sign-out">
+          <Icon name="tabler:logout-2" size="24" />
+          Sign Out
+        </NuxtLink>
+      </li>
+    </ul>
+  </div>
+
+  <button v-else :disabled="authStore.loading" class="btn btn-accent" @click="authStore.signIn()">
     Sign In w/
     <span v-if="authStore.loading" class="loading loading-spinner loading-md" />
     <Icon v-else name="tabler:brand-github-filled" size="24" />
