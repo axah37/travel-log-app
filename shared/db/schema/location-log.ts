4f5ee91661a65d254b6afef26ac611cec172c638
Abdul Ahmed <axah37@gmail.com>
2025-09-20 13:47:24 -0600

    #15 add better auth (#54)

--- shared/db/schema/location-log.ts
+++ shared/db/schema/location-log.ts
@@ -1,5 +1,6 @@
 import { int, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
+import { user } from "./auth";
 import { location } from "./location";
 
 export const locationLog = sqliteTable("location_log", {
@@ -9,6 +10,7 @@ export const locationLog = sqliteTable("location_log", {
   startedAt: int().notNull(),
   endedAt: int().notNull(),
   locationId: int().notNull().references(() => location.id),
+  userId: int().notNull().references(() => user.id),
   createdAt: int().notNull().$default(() => Date.now()),
   updatedAt: int().notNull().$default(() => Date.now()).$onUpdate(() => Date.now()),
 });
