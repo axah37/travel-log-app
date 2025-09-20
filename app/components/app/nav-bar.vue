7f15675f3ae2b7abbb69be0a591136792737be7c
Abdurahman Ahmed <axah37@gmail.com>
2025-09-19 22:27:53 -0600

    :sparkles: fix #12 toggle dark mode

--- app/components/app/nav-bar.vue
+++ app/components/app/nav-bar.vue
@@ -1,16 +1,19 @@
 <template>
   <div class="navbar bg-primary text-primary-content shadow-sm">
     <div class="flex-1">
-      <nuxt-link to="/" class="btn btn-ghost text-xl">
+      <NuxtLink to="/" class="btn btn-ghost text-xl">
         Travel Log
-      </nuxt-link>
+      </NuxtLink>
     </div>
     <div class="flex-none">
       <ul class="menu menu-horizontal px-1">
+        <li class="mx-4 my-auto">
+          <AppThemeSelector />
+        </li>
         <li>
-          <nuxt-link class="btn btn-accent">
+          <NuxtLink class="btn btn-accent">
             Sign In w/ <Icon name="tabler:brand-github-filled" size="24" />
-          </nuxt-link>
+          </NuxtLink>
         </li>
       </ul>
     </div>
