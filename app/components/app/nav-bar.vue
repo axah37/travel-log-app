3839a504170c1e988f6bfa4af48df360b53f4d68
Abdurahman Ahmed <axah37@gmail.com>
2025-09-19 22:04:38 -0600

    :sparkles: fixes #11: landing page

---
+++ app/components/app/nav-bar.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="navbar bg-primary text-primary-content shadow-sm">
+    <div class="flex-1">
+      <nuxt-link to="/" class="btn btn-ghost text-xl">
+        Travel Log
+      </nuxt-link>
+    </div>
+    <div class="flex-none">
+      <ul class="menu menu-horizontal px-1">
+        <li>
+          <nuxt-link class="btn btn-accent">
+            Sign In w/ <Icon name="tabler:brand-github-filled" size="24" />
+          </nuxt-link>
+        </li>
+      </ul>
+    </div>
+  </div>
+</template>
