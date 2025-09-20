cb5bf429f2bb2a95306bc9de533d9ca62af8e981
Abdul Ahmed <axah37@gmail.com>
2025-09-19 22:06:06 -0600

    :sparkles: fixes #11: landing page (#50)

--- app/app.vue
+++ app/app.vue
@@ -1,11 +1,5 @@
 <template>
-  <div>
-    <h1 class="text-3xl font-bold underline">
-      Hello world!
-    </h1>
-    <button class="btn btn-primary">
-      Button
-    </button>
-    <Icon name="tabler:a-b" size="36" />
-  </div>
+  <NuxtLayout>
+    <NuxtPage />
+  </NuxtLayout>
 </template>
