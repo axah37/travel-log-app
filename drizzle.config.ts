2752196d2a4ce69d31cd30ec96199fbc302bbb9d
Abdul <axah37@gmail.com>
2025-09-20 12:27:49 -0600

    change fs to linux

---
+++ drizzle.config.ts
@@ -0,0 +1,14 @@
+import "dotenv/config";
+import { defineConfig } from "drizzle-kit";
+
+import env from "./shared/env";
+
+export default defineConfig({
+  out: "./shared/db/migration",
+  schema: "./shared/db/schema/index.ts",
+  dialect: "turso",
+  dbCredentials: {
+    url: env.TURSO_DATABASE_URL,
+    authToken: env.TURSO_AUTH_TOKEN,
+  },
+});
