616c6de9a7d216150462d0d557841418db4e3f7f
Abdul <axah37@gmail.com>
2025-09-20 19:37:36 -0600

    19 protect routes

---
+++ server/middleware/auth.ts
@@ -0,0 +1,12 @@
+import { auth } from "~~/shared/auth";
+
+export default defineEventHandler(async (event) => {
+  if (event.path.startsWith("/dashboard")) {
+    const session = await auth.api.getSession({
+      headers: event.headers,
+    });
+    if (!session) {
+      await sendRedirect(event, "/", 302);
+    }
+  }
+});
