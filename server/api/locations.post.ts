d8778bbb59b77969e6da3a359143f640119bf012
Abdul <axah37@gmail.com>
2025-09-21 23:37:45 -0600

    #24 improve slug

--- server/api/locations.post.ts
+++ server/api/locations.post.ts
@@ -1,5 +1,10 @@
+import type { DrizzleError } from "drizzle-orm";
+
 import db from "~~/shared/db";
 import { insertLocation, location } from "~~/shared/db/schema";
+import { and, eq } from "drizzle-orm";
+import { customAlphabet } from "nanoid";
+import slugify from "slug";
 
 export default defineEventHandler(async (event) => {
   if (!event.context.user) {
@@ -22,11 +27,51 @@ export default defineEventHandler(async (event) => {
       data,
     }));
   }
+  const existingLocation = await db.query.location.findFirst({
+    where: and(eq(location.name, body.data.name), eq(location.userId, event.context.user.id)),
+  });
+  if (existingLocation) {
+    return sendError(event, createError({
+      statusCode: 409,
+      statusMessage: "A location with that name already exists",
+    }));
+  }
 
-  const [created] = await db.insert(location).values({
-    ...body.data,
-    slug: body.data.name.replaceAll(" ", "-").toLowerCase(),
-    userId: event.context.user.id,
-  }).returning();
-  return created;
+  try {
+    const [created] = await db.insert(location).values({
+      ...body.data,
+      slug: await generateUniqueSlug(body.data.name),
+      userId: event.context.user.id,
+    }).returning();
+    return created;
+  }
+  catch (e) {
+    const error = e as DrizzleError;
+    const cause = error.cause as unknown & { message?: string };
+    if (cause.message === "SQLITE_CONSTRAINT: SQLite error: UNIQUE constraint failed: location.slug") {
+      return sendError(event, createError({
+        statusCode: 409,
+        statusMessage: "Slug must be unique (the location name is used to generate the slug).",
+      }));
+    }
+    throw e;
+  }
 });
+
+async function generateUniqueSlug(name: string): Promise<string> {
+  const nanoid = customAlphabet("1234567890abcdefghijklmnopqrstuvwxyz", 5);
+  let slug = slugify(name);
+  let existing = !!(await db.query.location.findFirst({
+    where: eq(location.slug, slug),
+  }));
+  while (existing) {
+    const idSlug = slug += `-${nanoid()}`;
+    existing = !!(await db.query.location.findFirst({
+      where: eq(location.slug, idSlug),
+    }));
+    if (!existing) {
+      slug = idSlug;
+    }
+  }
+  return slug;
+}
